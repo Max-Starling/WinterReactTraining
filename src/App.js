0255d40e2372d545ccef331cb641d758cfea617e
Max Starling <17.max.starling@gmail.com>
2018-01-29 03:46:11 +0300

    Test canvas by using Konva

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* ----- Dependencies ----- */
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Konva from "konva";
 
 /* ----- Components ----- */
 import Header from "./components/Header/Header.js";
@@ -43,3 +44,537 @@ const Content = ({ match }) => {
 };
 
 export default App;
+
+// first we need to create a stage
+var stage = new Konva.Stage({
+  container: 'stage1',   // id of container <div>
+  width: 500,
+  height: 500,
+});
+
+// then create layer
+var layer = new Konva.Layer();
+
+// create our shape
+var pentagon = new Konva.RegularPolygon({
+  x: stage.getWidth() / 2,
+  y: stage.getHeight() / 2,
+  sides: 4,
+  radius: 60,
+  fill: 'red',
+  stroke: 'black',
+  strokeWidth: 2,
+  shadowColor: 'pink',
+  shadowOffsetX : 5,
+  shadowOffsetY : 5,
+  shadowBlur : 10,
+  opacity : 1,
+  name : 'red-quadro'
+});
+
+var pentagon2 = new Konva.RegularPolygon({
+  x: stage.getWidth() - 100,
+  y: stage.getHeight() - 100,
+  sides: 5,
+  radius: 60,
+  fill: 'green',
+  stroke: 'black',
+  strokeWidth: 2,
+  shadowColor: 'pink',
+  shadowOffsetX : 5,
+  shadowOffsetY : 5,
+  shadowBlur : 10,
+  opacity : 1,
+  name : 'red-quadro'
+});
+pentagon.draggable('true');
+pentagon2.draggable('true');
+
+pentagon.on('mouseout touchend', () => console.log('user input'));
+var triangle = new Konva.Shape({
+  sceneFunc: function(context) {
+    context.beginPath();
+    context.moveTo(20, 50);
+    context.lineTo(220, 80);
+    context.quadraticCurveTo(150, 100, 260, 170);
+    context.closePath();
+
+    // special Konva.js method
+    context.fillStrokeShape(this);
+  },
+  fill: '#00D2FF',
+  stroke: 'black',
+  strokeWidth: 4
+});
+// var redLine;
+// const redQuadro = layer.findOne('.red-quadro');
+// const triangle = layer.findOne('.red-quadro');
+function drawLink(coordX, coordY, viewX, viewY) {
+  const redLine = new Konva.Line({
+    points: [coordX, coordY, viewX, viewY],
+    stroke: 'black',
+    strokeWidth: 2,
+    lineCap: 'round',
+    lineJoin: 'round',
+    name: 'red-line'
+  });
+  layer.add(redLine);
+}
+drawLink(pentagon.getX(), pentagon.getY(), triangle.getX(), triangle.getY());
+drawLink(pentagon.getX(), pentagon.getY(), pentagon2.getX(), pentagon2.getY());
+layer.add(pentagon2);
+stage.add(layer);
+var text = new Konva.Text({
+  x: 10,
+  y: 10,
+  text: 'Static Layer',
+  fontSize: '30',
+  fontFamily: 'Calibri',
+  fill: 'black'
+});
+
+pentagon.on('xChange', (event) => {
+  // console.log('position change', event.target, event.currentTarget);
+  event.currentTarget.attrs.fill = "black";
+  // console.log(layer.find('.red-line'));
+  layer.find('.red-line')[0].attrs.points = [pentagon.getX(), pentagon.getY(), triangle.getX(), triangle.getY()];
+  layer.find('.red-line')[1].attrs.points = [pentagon.getX(), pentagon.getY(), pentagon2.getX(), pentagon2.getY()];
+  // drawLink(pentagon.getX(), pentagon.getY(), pentagon2.getX(), pentagon2.getY());
+  // layer.findOne('.red-quadro').attrs.fill = "black";
+});
+
+pentagon2.on('xChange', (event) => {
+  // layer.find('.red-line')[0].attrs.points = [pentagon.getX(), pentagon.getY(), triangle.getX(), triangle.getY()];
+  layer.find('.red-line')[1].attrs.points = [pentagon.getX(), pentagon.getY(), pentagon2.getX(), pentagon2.getY()];
+});
+
+pentagon.on('dragend', (event) => {
+  // console.log('drag stopped', event.target, event.currentTarget);
+  event.currentTarget.attrs.fill = "red";
+  console.log(event.currentTarget.getX());
+  console.log(pentagon.getX());
+  // drawLink(pentagon.getX(), pentagon.getY(), triangle.getX(), triangle.getY());
+  // redLine.attrs.points = [pentagon.getX(), pentagon.getY(), triangle.getX(), triangle.getY()];
+  layer.add(pentagon);
+  stage.add(layer);
+});
+
+
+text.cache();
+
+var staticLayer = new Konva.Layer();
+staticLayer.add(text);
+stage.add(staticLayer);
+// add the shape to the layer
+layer.add(pentagon);
+console.log(layer.find('.red-quadro')[0].attrs);
+
+
+// add the layer to the stage
+// stage.add(layer);
+
+// layer.add(triangle);
+stage.add(layer);
+
+var group = new Konva.Group({
+  draggable: true
+});
+
+// Font = require('canvas').Font,
+// path = require('path');
+// function fontFile(name) {
+//   return path.join(__dirname, '/Ubuntu/', name);
+// }
+
+// var font = new Font('Ubuntu', fontFile('Ubuntu-Regular.ttf'));
+// font.addFace(fontFile('Ubuntu-Bold.ttf'),   'bold');
+// font.addFace(fontFile('Ubuntu-Italic.ttf'), 'normal', 'italic');
+// font.addFace(fontFile('Ubuntu-BoldItalic.ttf'), 'bold', 'italic');
+
+
+
+var complexText = new Konva.Text({
+  x: 40,
+  y: 60,
+  text: 'COMPLEX TEXT\n\nAll the world\'s a stage, and all the men and women merely players. They have their exits and their entrances.',
+  fontSize: 18,
+  fontFamily: 'Cambria',
+  fill: '#555',
+  width: 400,
+  padding: 20,
+  align: 'center'
+});
+
+var rect = new Konva.Rect({
+  x: 40,
+  y: 60,
+  stroke: '#555',
+  strokeWidth: 3,
+  fill: '#ddd',
+  width: 400,
+  height: complexText.getHeight(),
+  shadowColor: 'black',
+  shadowBlur: 10,
+  shadowOffset: [10, 10],
+  shadowOpacity: 0.2,
+  cornerRadius: 10,
+  // draggable: true,
+});
+
+// layer.add(rect);
+// layer.add(complexText);
+group.add(rect);
+group.add(complexText);
+group.on('mouseover', () => document.body.style.cursor = 'pointer');
+group.on('mouseout', () => document.body.style.cursor = 'default');
+
+layer.add(group);
+
+// complexText.draggable('true');
+// complexText.on('dragged', (event) => {
+//   // event.currentTarget.attrs.fill = "black";
+//   rect.attrs.x = this.attrs.x;
+//   rect.attrs.y = this.attrs.y;
+//   layer.add(rect);
+//   layer.drow();
+//   // rect.draw();
+//   // layer.add(rect);
+//   // layer.add(complexText);
+//   stage.add(layer);
+//   console.log(rect.attrs);
+//   // console.log('qq');
+// });
+
+var group2 = new Konva.Group({
+  x: 120,
+  y: 40,
+  rotation: 20,
+  draggable: true,
+});
+
+var colors = ['red', 'orange', 'yellow'];
+
+for(var i = 0; i < 3; i++) {
+  var box = new Konva.Rect({
+      x: i * 30,
+      y: i * 18,
+      width: 100,
+      height: 50,
+      name: colors[i],
+      fill: colors[i],
+      stroke: 'black',
+      strokeWidth: 4
+  });
+  group2.add(box);
+}
+
+var tween;
+group.on('mousedown', function() {
+  // this.moveTo(group2);
+  if (tween) {
+    tween.destroy();  
+  }
+  tween = new Konva.Tween({
+    node: pentagon, 
+    duration: 1,
+    scaleX: Math.random() * 2,
+    scaleY: Math.random() * 2,
+    easing: Konva.Easings.ElasticEaseOut
+  }).play();
+});
+
+layer.add(group2);
+
+stage.add(layer);
+
+
+
+
+
+
+
+
+
+
+class Person {
+  constructor(fullName, url, targets, groups) {
+    this.name = fullName || "Unknown";
+    this.avatar = url || "default picture";
+    this.targets = targets || [];
+    this.groups = groups || [];
+  }
+  get getName() {
+    return this.name;
+  }
+  set setName(n) {
+    if (n) {
+      this.name = n;
+    }
+  }
+  get getAvatar() {
+    return this.avatar;
+  }
+  set setAvatar(a) {
+    if (a) {
+      this.avatar = a;
+    }
+  }
+  get getTargets() {
+    return this.targets;
+  }
+  set setTargets(t) {
+    if(t) {
+      this.targets = t;
+    }
+  }
+  get getGroups() {
+    return this.groups;
+  }
+  set setGroups(g) {
+    if(g) {
+      this.groups = g;
+    }
+  }
+  addTarget(t) {
+    if(t) {
+      this.targets.push(t);
+    }
+  }
+  addGroup(g) {
+    if(g) {
+      this.groups.push(g);
+    }
+  }
+}
+
+class TargetPreview {
+  constructor(name, generalProgress, remainingTime) {
+    this.name = name || "Target";
+    this.generalProgress = Number(generalProgress) || 0;
+    this.remainingTime = Number(remainingTime) || 24;
+  }
+  get getName() {
+    return this.name;
+  }
+  set setName(n) {
+    if (n) {
+      this.name = n;
+    }
+  }
+  get getGeneralProgress() {
+    return this.generalProgress;
+  }
+  set setGeneralProgress(gp) {
+    if (gp) {
+      this.generalProgress = gp;
+    }
+  }
+  get getRemainingTime() {
+    return this.remainingTime;
+  }
+  set setRemainingTime(rt) {
+    if (rt) {
+      this.remainingTime = rt;
+    }
+  }
+}
+
+class TargetDetail extends TargetPreview {
+  constructor(name, generalProgress, remainingTime, description) {
+    super(name, generalProgress,remainingTime);
+    this.description = description || "Description";
+  }
+  get getDescription() {
+    return this.description;
+  }
+  set setDescription(d) {
+    if (d) {
+      this.description = d;
+    }
+  }
+}
+
+const p = new Person('Alen Stone');
+const t = new TargetDetail('testTarget', 0.3, 1, "It's awesome target.");
+
+p.setAvatar = "src/resources/images/logo.svg";
+
+p.addTarget(t);
+// console.log(p.getTargets);
+
+var mainStage = new Konva.Stage({
+  container: 'stage2',
+  width: window.innerWidth,
+  height: 500,
+});
+
+
+function createPerson({ positionX, positionY, name, avatar }) {
+  const personGroup = new Konva.Group({
+    x: positionX,
+    y: positionY,
+    draggable: true,
+  });
+
+  const personCardText = new Konva.Text({
+    text: `${name}\n${avatar}`, 
+    fontSize: 18,
+    fontFamily: 'Cambria',
+    fill: '#555',
+    width: 300,
+    padding: 20,
+    align: 'center'
+  });
+  
+  const personCardBackground = new Konva.Rect({
+    stroke: 'f7f7f7',
+    strokeWidth: 0.15,
+    fill: '#f7f7f7',
+    width: 300,
+    height: personCardText.getHeight() + 100,
+    shadowColor: 'black',
+    shadowBlur: 5,
+    shadowOffset: {x : 2, y : 2},
+    shadowOpacity: 0.5,
+  });
+  personGroup.add(personCardBackground);
+  personGroup.add(personCardText);
+  return personGroup;
+}
+
+function createTarget({ positionX, 
+                        positionY, 
+                        name, 
+                        genetalProgress, 
+                        remainingTime, 
+                        description }) {
+  const targetGroup = new Konva.Group({
+    x: positionX,
+    y: positionY,
+    draggable: true,
+  });
+
+  const targetCardText = new Konva.Text({
+    x: -150,
+    y: -50,
+    text: `${name}\n${genetalProgress}\n${remainingTime}\n${description}`, 
+    fontSize: 18,
+    fontFamily: 'Cambria',
+    fill: '#555',
+    width: 300,
+    padding: 20,
+    align: 'center'
+  });
+
+  const targetBackground = new Konva.Circle({
+    x: 0,
+    y: 0,
+    stroke: 'f7f7f7',
+    strokeWidth: 0.07,
+    fill: '#f7f7f7',
+    // radius: 40,
+    height: targetCardText.getHeight() + 70,
+    shadowColor: 'black',
+    shadowBlur: 5,
+    shadowOffset: {x : 2, y : 2},
+    shadowOpacity: 0.5,
+  });
+  
+  targetGroup.add(targetBackground);
+  targetGroup.add(targetCardText);
+
+  return targetGroup;
+}
+var personLayer = new Konva.Layer();
+
+personLayer.add(createPerson({
+  positionX: stage.getHeight() / 2,
+  positionY: 5,
+  name: p.getName, 
+  avatar: p.getAvatar,
+}));
+
+const Dan = createPerson({
+  positionX: stage.getHeight() / 2 + 400,
+  positionY: 5,
+  name: 'Dan KR', 
+  avatar: 'krasivo.svg',
+});
+
+
+const targetLayer = new Konva.Layer();
+
+const targetDan = createTarget({
+  positionX: stage.getHeight() / 2,
+  positionY: 100,
+  name: t.getName,
+  remainingTime: t.getRemainingTime,
+  genetalProgress: t.getGeneralProgress,
+  description: t.getDescription,
+});
+// console.log(tar);
+
+
+
+const lineLayer = new Konva.Layer();
+
+
+function drawLine(coordX, coordY, viewX, viewY) {
+  const linkLine = new Konva.Line({
+    points: [coordX, coordY, viewX, viewY],
+    stroke: 'black',
+    strokeWidth: 1,
+    lineCap: 'round',
+    lineJoin: 'round',
+    name: 'link-line'
+  });
+  lineLayer.add(linkLine);
+}
+
+console.log(targetDan, targetDan.y, Dan, Dan.y);
+drawLine(targetDan.attrs.x, targetDan.attrs.y, Dan.attrs.x + 150, Dan.attrs.y + 100);
+
+
+targetDan.on('xChange', (event) => {
+  lineLayer.find('.link-line')[0].attrs.points = [targetDan.attrs.x, targetDan.attrs.y, Dan.attrs.x + 150, Dan.attrs.y + 100];
+  mainStage.add(lineLayer);
+  mainStage.add(personLayer);
+  mainStage.add(targetLayer);
+  // layer.find('.link-line')[1].attrs.points = [targetDan.attrs.x, targetDan.attrs.y, Dan.attrs.x + 150, Dan.attrs.y + 100];
+});
+
+Dan.on('xChange', (event) => {
+  lineLayer.find('.link-line')[0].attrs.points = [targetDan.attrs.x, targetDan.attrs.y, Dan.attrs.x + 150, Dan.attrs.y + 100];
+  mainStage.add(lineLayer);
+  mainStage.add(personLayer);
+  mainStage.add(targetLayer);
+  // layer.find('.link-line')[1].attrs.points = [targetDan.attrs.x, targetDan.attrs.y, Dan.attrs.x + 150, Dan.attrs.y + 100];
+});
+
+personLayer.add(Dan);
+targetLayer.add(targetDan);
+
+mainStage.add(lineLayer);
+mainStage.add(personLayer);
+mainStage.add(targetLayer);
+
+
+
+// var imageObj = new Image();
+// imageObj.onload = function() {
+//   console.log('qq');
+//   var yoda = new Konva.Image({
+//     x: 50,
+//     y: 50,
+//     image: imageObj,
+//     width: 106,
+//     height: 118
+//   });
+
+//   // add the shape to the layer
+//   personLayer.add(yoda);
+
+//   // add the layer to the stage
+//   stage.add(personLayer);
+// };
+// imageObj.src = "./resources/images/header.png";
